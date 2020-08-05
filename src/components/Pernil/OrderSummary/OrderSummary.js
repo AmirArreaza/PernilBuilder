e3cbed3a497653d2fd51e5c154db38277d304d3d
AmirArreaza <ajaarreazav@gmail.com>
2020-08-05 15:32:13 +0100

    Added the Cancel and continue buttons to the order summary

--- src/components/Pernil/OrderSummary/OrderSummary.js
+++ src/components/Pernil/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Auxiliary from "../../../hoc/Auxiliary";
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
@@ -18,6 +19,8 @@ const orderSummary = (props) => {
       <p>A delicious pernil with:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
     </Auxiliary>
   );
 };
--- src/containers/PernilBuilder/PernilBuilder.js
+++ src/containers/PernilBuilder/PernilBuilder.js
@@ -1,120 +1,130 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Auxiliary from '../../hoc/Auxiliary';
-import Pernil from '../../components/Pernil/Pernil'
-import BuildControls from '../../components/Pernil/BuildControls/BuildControls';
-import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Pernil/OrderSummary/OrderSummary'
+import Auxiliary from "../../hoc/Auxiliary";
+import Pernil from "../../components/Pernil/Pernil";
+import BuildControls from "../../components/Pernil/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Pernil/OrderSummary/OrderSummary";
 
 const INGREDIENT_PRICES = {
-    salad: 0.5,
-    cheese: 0.4,
-    meat: 1.3,
-    tomatoes: 0.7
-}
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  tomatoes: 0.7,
+};
 
-class PernilBuilder extends Component{
+class PernilBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      tomatoes: 0,
+      cheese: 0,
+      meat: 0,
+    },
+    totalPrice: 4,
+    purchaseable: false,
+    purchasing: false,
+  };
 
-    state = {
-        ingredients: {
-            salad: 0,
-            tomatoes: 0,
-            cheese: 0,
-            meat: 0
-        },
-        totalPrice: 4,
-        purchaseable: false,
-        purchasing: false
-    }
+  updatePurchaseState(ingredients) {
+    const sum = Object.keys(ingredients)
+      .map((igKey) => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+    console.log(sum);
+    this.setState({ purchaseable: sum > 0 });
+  }
 
-    updatePurchaseState(ingredients){
-        const sum = Object.keys(ingredients)
-            .map(igKey => {
-                return ingredients[igKey];
-            })
-            .reduce((sum, el) => {
-                return sum + el;
-            }, 0);
-        console.log(sum);
-        this.setState({ purchaseable: sum > 0});
-    }
+  addIngridientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCounted = oldCount + 1;
 
-    addIngridientHandler = (type) => {
-        const oldCount = this.state.ingredients[type];
-        const updatedCounted = oldCount + 1;
-
-        const updatedIngredients = {
-            ...this.state.ingredients
-        };
-
-        updatedIngredients[type] = updatedCounted;
-        const priceAddition = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice + priceAddition;
-
-        this.setState({ 
-            totalPrice: newPrice, 
-            ingredients: updatedIngredients 
-        });
-        this.updatePurchaseState(updatedIngredients);
-    }
-
-    removeIngridientHandler = (type) => {
-        const oldCount = this.state.ingredients[type];
-
-        if(oldCount <= 0) return;
-
-        const updatedCounted = oldCount - 1;
-
-        const updatedIngredients = {
-            ...this.state.ingredients
-        };
-
-        updatedIngredients[type] = updatedCounted;
-        const priceDeduction = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice - priceDeduction;
-
-        this.setState({ 
-            totalPrice: newPrice, 
-            ingredients: updatedIngredients 
-        });
-        this.updatePurchaseState(updatedIngredients);
-    }
-
-    purchaseHandler = () =>{
-        this.setState({ purchasing: true })
-    }
-
-    purchaseCancelHandler = () => {
-        this.setState({ purchasing: false});
-    }
-
-    render(){
-
-        const disabledInfo = {
-            ...this.state.ingredients
-        };
-        for(let key in disabledInfo){
-            disabledInfo[key] = disabledInfo[key] <= 0;
-        } 
-
-        return(
-            <Auxiliary>
-                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
-                </Modal>
-                <Pernil ingredients={this.state.ingredients}/>
-                <BuildControls 
-                    ingredientAdded={this.addIngridientHandler}
-                    ingredientRemoved={this.removeIngridientHandler}
-                    disabled={disabledInfo}
-                    purchaseable={this.state.purchaseable}
-                    price={this.state.totalPrice}
-                    ordered={this.purchaseHandler}/>
-            </Auxiliary>
-        );
+    const updatedIngredients = {
+      ...this.state.ingredients,
     };
+
+    updatedIngredients[type] = updatedCounted;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+
+    this.setState({
+      totalPrice: newPrice,
+      ingredients: updatedIngredients,
+    });
+    this.updatePurchaseState(updatedIngredients);
+  };
+
+  removeIngridientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+
+    if (oldCount <= 0) return;
+
+    const updatedCounted = oldCount - 1;
+
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+
+    updatedIngredients[type] = updatedCounted;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+
+    this.setState({
+      totalPrice: newPrice,
+      ingredients: updatedIngredients,
+    });
+    this.updatePurchaseState(updatedIngredients);
+  };
+
+  purchaseHandler = () => {
+    this.setState({ purchasing: true });
+  };
+
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
+  };
+
+  purchaseContinueHandler = () => {
+    alert("Continue");
+  };
+
+  render() {
+    const disabledInfo = {
+      ...this.state.ingredients,
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+
+    return (
+      <Auxiliary>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCanceled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+          />
+        </Modal>
+        <Pernil ingredients={this.state.ingredients} />
+        <BuildControls
+          ingredientAdded={this.addIngridientHandler}
+          ingredientRemoved={this.removeIngridientHandler}
+          disabled={disabledInfo}
+          purchaseable={this.state.purchaseable}
+          price={this.state.totalPrice}
+          ordered={this.purchaseHandler}
+        />
+      </Auxiliary>
+    );
+  }
 }
 
 export default PernilBuilder;
