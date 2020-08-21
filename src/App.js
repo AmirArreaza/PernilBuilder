4319e5f49b5c9e9ce47b8292fa658f6fc672b3d0
AmirArreaza <ajaarreazav@gmail.com>
2020-08-21 23:52:51 +0100

    Added the Orders and Order components to display the orders data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import PernilBuilder from "./containers/PernilBuilder/PernilBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   state = {
@@ -23,6 +24,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout}></Route>
+            <Route path="/orders" component={Orders}></Route>
             <Route path="" exact component={PernilBuilder}></Route>
           </Switch>
         </Layout>
