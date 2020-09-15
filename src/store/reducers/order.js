7d53b12e9c429eb1fff0434ec2d66867d50206fb
AmirArreaza <ajaarreazav@gmail.com>
2020-09-15 14:51:30 +0100

    Reducers refactored

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   orders: [],
@@ -6,50 +7,55 @@ const initialState = {
   purchased: false,
 };
 
+const purchasePernilSuccess = (state, action) => {
+  const newOrder = updateObject(action.orderData, {
+    id: action.orderId,
+  });
+  return updateObject(state, {
+    ...state,
+    loading: false,
+    orders: newOrder,
+    purchased: true,
+  });
+};
+const purchasePernilFail = (state, action) =>
+  updateObject(state, { loading: false });
+const purchasePernilStart = (state, action) =>
+  updateObject(state, { loading: true });
+const purchaseInit = (state, action) => {
+  return updateObject(state, {
+    ...state,
+    purchased: false,
+  });
+};
+const fetchOrdersStart = (state, action) =>
+  updateObject(state, { loading: true });
+const fetchOrdersSuccess = (state, action) => {
+  return updateObject(state, {
+    ...state,
+    orders: action.orders,
+    loading: false,
+  });
+};
+const fetchOrdersFailed = (state, action) =>
+  updateObject(state, { loading: false });
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.PURCHASE_PERNIL_SUCCESS:
-      const newOrder = {
-        ...action.orderData,
-        id: action.orderId,
-      };
-      return {
-        ...state,
-        loading: false,
-        orders: state.orders.concat(newOrder),
-        purchased: true,
-      };
+      return purchasePernilSuccess(state, action);
     case actionTypes.PURCHASE_PERNIL_FAIL:
-      return {
-        ...state,
-        loading: false,
-      };
+      return purchasePernilFail(state, action);
     case actionTypes.PURCHASE_PERNIL_START:
-      return {
-        ...state,
-        loading: true,
-      };
+      return purchasePernilStart(state, action);
     case actionTypes.PURCHASE_INIT:
-      return {
-        ...state,
-        purchased: false,
-      };
+      return purchaseInit(state, action);
     case actionTypes.FETCH_ORDERS_START:
-      return {
-        ...state,
-        loading: true,
-      };
+      return fetchOrdersStart(state, action);
     case actionTypes.FETCH_ORDERS_SUCCESS:
-      return {
-        ...state,
-        orders: action.orders,
-        loading: false,
-      };
+      return fetchOrdersSuccess(state, action);
     case actionTypes.FETCH_ORDERS_FAILED:
-      return {
-        ...state,
-        loading: false,
-      };
+      return fetchOrdersFailed(state, action);
     default:
       return state;
   }
--- src/store/reducers/pernilBuilder.js
+++ src/store/reducers/pernilBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   ingredients: null,
@@ -13,43 +14,56 @@ const INGREDIENT_PRICES = {
   tomatoes: 0.7,
 };
 
+const addIngredient = (state, action) => {
+  const addedIngredient = {
+    [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+  };
+  const adedIngredients = updateObject(state.ingredients, addedIngredient);
+  const updatedAddedState = {
+    ingredients: adedIngredients,
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+  };
+  return updateObject(state, updatedAddedState);
+}
+
+const removeIngredient = (state, action) => {
+  const addedIngredient = {
+    [action.ingredientName]: state.ingredients[action.ingredientName] -1,
+  };
+  const adedIngredients = updateObject(state.ingredients, addedIngredient);
+  const updatedAddedState = {
+    ingredients: adedIngredients,
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+  };
+  return updateObject(state, updatedAddedState);
+}
+
+const setIngredient = (state, action) => {
+  return updateObject(state, {
+    ...state,
+    ingredients: {
+      salad: action.ingredients.salad,
+      tomatoes: action.ingredients.tomatoes,
+      cheese: action.ingredients.cheese,
+      meat: action.ingredients.meat,
+    },
+    totalPrice: 4,
+    error: false,
+  });
+};
+
+const fetchIngredientFailed = (state, action) => updateObject(state, { ...state, error: true });
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENTS:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] + 1, //ES6 way to change a property
-        },
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
-      };
+      return addIngredient(state, action);
     case actionTypes.REMOVE_INGREDIENTS:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
-        },
-        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
-      };
+      return removeIngredient(state, action);
     case actionTypes.SET_INGREDIENTS:
-      return {
-        ...state,
-        ingredients: {
-          salad: action.ingredients.salad,
-          tomatoes: action.ingredients.tomatoes,
-          cheese: action.ingredients.cheese,
-          meat: action.ingredients.meat
-        },
-        totalPrice: 4,
-        error: false
-      };
+      return setIngredient(state, action);
     case actionTypes.FETCH_INGREDIENTS_FAILED:
-      return {
-        ...state,
-        error: true,
-      };
+      return fetchIngredientFailed(state, action);
     default:
       return state;
   }
