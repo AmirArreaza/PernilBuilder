cf6cf38c957f97514769e05dedbd467d2810116e
AmirArreaza <ajaarreazav@gmail.com>
2020-09-29 13:53:47 +0100

    Added the Sage Middleware

--- src/index.js
+++ src/index.js
@@ -9,10 +9,12 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import createSagaMiddleware from 'redux-saga';
 
 import pernilBuilderReducer from "./store/reducers/pernilBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
+import { logoutSaga } from './store/sagas/auth';
 
 const composeEnhancers =
   process.env.NODE_ENV === "development"
@@ -25,10 +27,14 @@ const rootReducer = combineReducers({
   auth: authReducer,
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
 );
+
+sagaMiddleware.run(logoutSaga);
 
 //Provider should wrap everything!!
 
--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -3,7 +3,7 @@ import { put } from "redux-saga/effects";
 import * as actionTypes from "../actions/actionTypes";
 
 // * Converts the function into a generator
-function* logout(action) {
+export function* logoutSaga(action) {
   yield localStorage.removeItem("token");
   yield localStorage.removeItem("expirationDate");
   yield localStorage.removeItem("userId");
