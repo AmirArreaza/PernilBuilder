30fbf409310ba8078d38a291edff30d16df31453
AmirArreaza <ajaarreazav@gmail.com>
2020-10-07 10:51:10 +0100

    Changed the Layout component to a functional component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 
 import Auxiliary from "../Auxiliary/Auxiliary";
@@ -6,38 +6,32 @@ import classes from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
-class Layout extends Component {
-  state = {
-    showSideDrawer: false,
+const Layout = (props) => {
+  const [sideDrawerIsVisible, setSideDrawerIsVisible] = useState(false);
+
+  const sideDrawerClosedHandler = () => {
+    setSideDrawerIsVisible(false);
   };
 
-  sideDrawerClosedHandler = () => {
-    this.setState({ showSideDrawer: false });
+  const sideDrawerToggleHanlder = () => {
+    setSideDrawerIsVisible(!sideDrawerIsVisible);
   };
 
-  sideDrawerToggleHanlder = () => {
-    this.setState((prevState) => {
-      return { showSideDrawer: !prevState.showSideDrawer };
-    });
-  };
-
-  render() {
-    return (
-      <Auxiliary>
-        <Toolbar
-          drawerOpenedClicked={this.sideDrawerToggleHanlder}
-          isAuth={this.props.isAuthenticated}
-        />
-        <SideDrawer
-          open={this.state.showSideDrawer}
-          closed={this.sideDrawerClosedHandler}
-          isAuth={this.props.isAuthenticated}
-        />
-        <main className={classes.Content}>{this.props.children}</main>
-      </Auxiliary>
-    );
-  }
-}
+  return (
+    <Auxiliary>
+      <Toolbar
+        drawerOpenedClicked={sideDrawerToggleHanlder}
+        isAuth={props.isAuthenticated}
+      />
+      <SideDrawer
+        open={sideDrawerIsVisible}
+        closed={sideDrawerClosedHandler}
+        isAuth={props.isAuthenticated}
+      />
+      <main className={classes.Content}>{props.children}</main>
+    </Auxiliary>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
