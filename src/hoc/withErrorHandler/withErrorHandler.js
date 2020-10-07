2013e5920d72ecb7ceeece914171e2b5e146bf3f
AmirArreaza <ajaarreazav@gmail.com>
2020-10-07 15:17:58 +0100

    Added a custom hook to handle the http errors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,40 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import Modal from "../../components/UI/Modal/Modal";
 import Auxiliary from "../Auxiliary/Auxiliary";
+import useHttpErrorHandler from '../../hooks/http-error-handler';
 
 const withErrorHandler = (WrappedComponent, axios) => {
   return function (props) {
-    const [error, setError] = useState(null);
-
-    const reqInterceptor = axios.interceptors.request.use((request) => {
-      setError(null);
-      return request;
-    });
-    const resInterceptor = axios.interceptors.response.use(
-      function (response) {
-        return response;
-      },
-      function (err) {
-        setError(err);
-        return Promise.reject(err);
-      }
-    );
-
-    useEffect(() => {
-      return () => {
-        axios.interceptors.request.eject(reqInterceptor);
-        axios.interceptors.response.eject(resInterceptor);
-      };
-    }, [reqInterceptor, resInterceptor]);
-
-    const errorConfirmedHandler = () => {
-      setError(null);
-    };
+    const [error, clearError] = useHttpErrorHandler(axios);
+    
 
     return (
       <Auxiliary>
-        <Modal show={error} modalClosed={errorConfirmedHandler}>
+        <Modal show={error} modalClosed={clearError}>
           {error ? error.message : null}
         </Modal>
         <WrappedComponent {...props} />
