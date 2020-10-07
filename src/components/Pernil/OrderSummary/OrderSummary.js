ea3c46b7b05eb3737b75849c5e6d3226ecc14591
AmirArreaza <ajaarreazav@gmail.com>
2020-10-07 15:10:20 +0100

    Adjusted PernilIngredient, Modal and OrderSummary components with Hooks. Added React.memo to optimize the Modal

--- src/components/Pernil/OrderSummary/OrderSummary.js
+++ src/components/Pernil/OrderSummary/OrderSummary.js
@@ -1,39 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 
 import Auxiliary from "../../../hoc/Auxiliary/Auxiliary";
 import Button from "../../UI/Button/Button";
 
-class OrderSummary extends Component {
-  
-  
-  render() {
-    const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => {
-      return (
-        <li key={igKey}>
-          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-          {this.props.ingredients[igKey]}
-        </li>
-      );
-    });
-
+const OrderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
     return (
-      <Auxiliary>
-        <h3>Your Order</h3>
-        <p>A delicious pernil with:</p>
-        <ul>{ingredientSummary}</ul>
-        <p>Continue to Checkout?</p>
-        <p>
-          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
-        </p>
-        <Button btnType="Danger" clicked={this.props.purchaseCanceled}>
-          CANCEL
-        </Button>
-        <Button btnType="Success" clicked={this.props.purchaseContinued}>
-          CONTINUE
-        </Button>
-      </Auxiliary>
+      <li key={igKey}>
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+        {props.ingredients[igKey]}
+      </li>
     );
-  }
-}
+  });
+
+  return (
+    <Auxiliary>
+      <h3>Your Order</h3>
+      <p>A delicious pernil with:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to Checkout?</p>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
+      <Button btnType="Danger" clicked={props.purchaseCanceled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </Auxiliary>
+  );
+};
 
 export default OrderSummary;
